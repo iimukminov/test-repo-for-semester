ba7724e08dff39cd84ab80d96e77d353c2e67950
iimukminov <iimukminov06@gmail.com>
2026-05-21 20:20:50 +0300

    implement RoadmapsController using generated OpenApi interface

---
+++ src/main/java/com/mukminov/controller/RoadmapController.java
@@ -0,0 +1,46 @@
+package com.mukminov.controller;
+
+import com.mukminov.api.generated.api.RoadmapsApi;
+import com.mukminov.api.generated.dto.RoadmapCreateDto;
+import com.mukminov.api.generated.dto.RoadmapDto;
+import com.mukminov.service.RoadmapService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class RoadmapController implements RoadmapsApi {
+
+    private final RoadmapService roadmapService;
+
+    @Override
+    public ResponseEntity<RoadmapDto> createRoadmap(RoadmapCreateDto roadmapCreateDto) {
+        RoadmapDto created = roadmapService.createRoadmap(
+                roadmapCreateDto.getMentorId(),
+                roadmapCreateDto.getMenteeId(),
+                roadmapCreateDto.getTitle(),
+                roadmapCreateDto.getDescription()
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteRoadmap(Long id) {
+        roadmapService.deleteRoadmap(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @Override
+    public ResponseEntity<RoadmapDto> getRoadmapById(Long id) {
+        return ResponseEntity.ok(roadmapService.getRoadmapById(id));
+    }
+
+    @Override
+    public ResponseEntity<List<RoadmapDto>> getRoadmaps(Long mentorId, Long menteeId) {
+        return ResponseEntity.ok(roadmapService.getRoadmaps(mentorId, menteeId));
+    }
+}
