f4be2b156b76aa80bf1e8c6b293c1f8bd10276e2
iimukminov <iimukminov06@gmail.com>
2026-05-21 21:25:13 +0300

    implement ReviewFeedbackController using generated OpenApi interface

---
+++ src/main/java/com/mukminov/controller/ReviewFeedbackController.java
@@ -0,0 +1,28 @@
+package com.mukminov.controller;
+
+import com.mukminov.api.generated.api.ReviewFeedbackApi;
+import com.mukminov.api.generated.dto.ReviewFeedbackCreateDto;
+import com.mukminov.api.generated.dto.ReviewFeedbackDto;
+import com.mukminov.service.ReviewFeedbackService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ReviewFeedbackController implements ReviewFeedbackApi {
+
+    private final ReviewFeedbackService reviewFeedbackService;
+
+    @Override
+    public ResponseEntity<ReviewFeedbackDto> addFeedbackToStep(Long stepId, ReviewFeedbackCreateDto reviewFeedbackCreateDto) {
+        ReviewFeedbackDto created = reviewFeedbackService.addFeedbackToStep(stepId, reviewFeedbackCreateDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @Override
+    public ResponseEntity<ReviewFeedbackDto> getFeedbackByStepId(Long stepId) {
+        return ResponseEntity.ok(reviewFeedbackService.getFeedbackByStepId(stepId));
+    }
+}
